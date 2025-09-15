fe4d7b0c49ea519e6c0e6f9463a8866936c782fe
kathiravan2002 <kathiravankathir910@gmail.com>
2025-09-15 15:44:46 +0530

    crt

--- src/shared/components/Home.jsx
+++ src/shared/components/Home.jsx
@@ -97,12 +97,12 @@ function Home() {
                     ))}
                 </div>
             </section>
-            <div className="max-w-[1600px] mx-auto">
+            <div className="max-w-[1450px] mx-auto">
                 <div className=" pt-10 space-y-10   px-4">
                 <h2 className="text-gray-800 poppins-semibold text-[34px] text-center">Indexed <span className="text-[#fcac45] ">Journals</span></h2>
                     <p className="text-center inter-medium opacity-85 text-[18px] leading-[30px] max-w-[1000px] mx-auto">The Selective accepted papers of our International Conference on Computer Science Innovations will be recommended to publish in the following index & publisher Journals.</p>
                 </div>
-                <Marquee direction='left' gradient pauseOnClick pauseOnHover>
+                <Marquee direction='left'  pauseOnClick pauseOnHover>
                     <div className="  flex  pb-10 pt-10 ">
                         {imageItems.map((item, index) => (
                             <div key={index} className=" flex justify-center mr-10  py-5" >
